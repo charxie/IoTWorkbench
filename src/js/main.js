5aad15243501ebc3557d96aa26ce8cc647dc86ce
Charles Xie <charxie@gmail.com>
2019-02-20 15:37:01 -0500

    firebase!

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,13 @@
 
 let canvas;
 let gridSize = 50;
+let redLedState;
+let greenLedState;
+let blueLedState;
+let redLedImage;
+let greenLedImage;
+let blueLedImage;
+let database;
 
 function init() {
 
@@ -22,16 +29,19 @@ function init() {
   firebase.initializeApp(config);
 
   // Get a reference to the database service
-  let database = firebase.database();
+  database = firebase.database();
   console.log(database);
-  database.ref().on("value", function(snapshot) {
-    snapshot.forEach(function(child) {
+  database.ref().on("value", function (snapshot) {
+    snapshot.forEach(function (child) {
       let childData = child.val();
-      console.log(childData);
+      blueLedState = childData.blueLed;
+      console.log("Firebase: " + blueLedState);
+      draw();
     });
   });
 
   canvas = document.getElementById("board");
+  canvas.addEventListener("click", click, false);
   resizeCanvas();
   draw();
 
@@ -44,6 +54,16 @@ function init() {
 
 }
 
+function click(e) {
+  let blueLedClicked = e.x > 310 && e.x < 310 + blueLedImage.width && e.y > 500 && e.y < 500 + blueLedImage.height;
+  if (blueLedClicked) {
+    blueLedState = !blueLedState;
+    draw();
+    console.log("clicked: " + e.x + "," + e.y + ", " + blueLedImage.src);
+    database.ref('rainbow_hat').update({ blueLed: blueLedState });
+  }
+}
+
 function resize() {
 
 }
@@ -51,6 +71,8 @@ function resize() {
 function draw() {
 
   let ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   ctx.beginPath();
   ctx.strokeStyle = "LightSkyBlue";
   for (let i = 1; i <= canvas.height / gridSize; i++) {
@@ -75,23 +97,21 @@ function draw() {
     ctx.drawImage(buzzer, 10, 500);
   };
 
-  let redLed = new Image();
-  redLed.src = 'img/red_led_light_off.png';
-  redLed.onload = function () {
-    ctx.drawImage(redLed, 110, 500);
+  redLedImage = new Image();
+  redLedImage.src = 'img/red_led_light_off.png';
+  redLedImage.onload = function () {
+    ctx.drawImage(redLedImage, 110, 500);
   };
 
-  let greenLed = new Image();
-  greenLed.src = 'img/green_led_light_off.png';
-  greenLed.onload = function () {
-    ctx.drawImage(greenLed, 210, 500);
+  greenLedImage = new Image();
+  greenLedImage.src = 'img/green_led_light_off.png';
+  greenLedImage.onload = function () {
+    ctx.drawImage(greenLedImage, 210, 500);
   };
 
-  var blueLed = new Image();
-  blueLed.src = 'img/blue_led_light_off.png';
-  blueLed.onload = function () {
-    ctx.drawImage(blueLed, 310, 500);
-  };
+  blueLedImage = document.getElementById(blueLedState ? 'blue_led_light_on' : 'blue_led_light_off');
+  ctx.drawImage(blueLedImage, 310, 500);
+  console.log("img src = " + blueLedImage.src);
 
 }
 
