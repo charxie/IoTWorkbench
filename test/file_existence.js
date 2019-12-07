b22ba88e1baaff68622275f139e69f1553a4a146
Charles Xie <charxie@gmail.com>
2019-12-06 21:13:15 -0500

    user database

--- test/file_existence.js
+++ test/file_existence.js
@@ -46,7 +46,7 @@ const expectedFilesInDistDir = [
   'index.html',
 
   'js/',
-  'js/main.js',
+  'js/Main.js',
   'js/plugins.js',
   'js/vendor/',
   `js/vendor/jquery-${pkg.devDependencies.jquery}.min.js`,
