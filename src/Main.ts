b759fa6ae07116014afb1da723ea8a7b7b637240
Charles Xie <charxie@gmail.com>
2020-03-09 23:08:49 -0400

    layout fix

--- src/Main.ts
+++ src/Main.ts
@@ -31,6 +31,7 @@ import {Flowchart} from "./blocks/Flowchart";
 import {BlockElementsPanel} from "./blocks/ui/BlockElementsPanel";
 import {createContextMenusForBlocks} from "./BlockContextMenusMaker";
 import {createContextMenusForModels} from "./ModelContextMenusMaker";
+import {Util} from "./Util";
 
 declare global {
   interface CanvasRenderingContext2D {
@@ -111,9 +112,11 @@ window.onload = function () {
   document.getElementById("sign-in-label").innerHTML = "Hi, " + user.firstName;
   document.getElementById("name-label").innerHTML = Constants.Software.name;
   document.getElementById("tagline-label").innerHTML = Constants.Software.tagline;
-  document.getElementById('credit').innerHTML = social + "<div class='horizontal-divider'></div>"
-    + Constants.Software.name + " Version " + Constants.Software.version
-    + ", created by <a href='https://charxie.github.io/' style='text-decoration: none;'>Dr. Charles Xie</a>, " + new Date().getFullYear();
+  if (Util.getOS() != "Android") {
+    document.getElementById('credit').innerHTML = social + "<div class='horizontal-divider'></div>"
+      + Constants.Software.name + " Version " + Constants.Software.version
+      + ", created by <a href='https://charxie.github.io/' style='text-decoration: none;'>Dr. Charles Xie</a>, " + new Date().getFullYear();
+  }
 
   let examples = new Examples();
   document.getElementById("main-page-previous-tutorial-button").onclick = function () {
